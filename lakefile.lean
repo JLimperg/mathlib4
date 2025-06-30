db082585d5ad69f44b5f3cd70c73146f32a6a909
Jannis Limperg <limperg@amazon.com>
2025-06-30 12:53:50 +0000

    Fix build with rpinf

--- lakefile.lean
+++ lakefile.lean
@@ -49,7 +49,7 @@ abbrev mathlibOnlyLinters : Array LeanOption := #[
   ⟨`aesop.collectStatsSkipTypes, true⟩,
   ⟨`aesop.collectStatsSkipImplicitArguments, true⟩,
   ⟨`aesop.collectStatsRpinf, true⟩,
-  ⟨`maxHeartbeats, .ofNat 200000⟩  --
+  ⟨`maxHeartbeats, .ofNat 1000000000000⟩  --
 ]
 
 /-- These options are passed as `leanOptions` to building mathlib, as well as the
